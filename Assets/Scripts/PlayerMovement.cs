a35fe3e3045f470dddc4ed4a76a56a78d1856ae7
stemguest <stemguest@itcs.ccny.lan>
2024-10-25 15:59:18 -0400

    10/25

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,22 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D _rigidbody2D;
+
+    private float _xSpeed;
+    private float _ySpeed;
+    private void Start()
+    {
+        _rigidbody2D = GetConponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        _xSpeed = Input.GetAxis("Horizontal")
+    }
 
 }
+   
+    
